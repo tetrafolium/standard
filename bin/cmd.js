ba5391f0c127589a1f189cf3fb1cc6962f3fee1e
Inspecode <inspecode-bot@rocro.com>
2020-11-20 08:46:19 +0000

    Fix ClangFormat errors

--- bin/cmd.js
+++ bin/cmd.js
@@ -6,6 +6,8 @@ var minor = parseInt(match[2], 10)
 
 if (major >= 9 || (major === 8 && minor >= 10)) {
   require('standard-engine').cli(require('../options'))
-} else {
-  console.error('standard: Node 8.10.0 or greater is required. `standard` did not run.')
+}
+else {
+  console.error(
+      'standard: Node 8.10.0 or greater is required. `standard` did not run.')
 }
