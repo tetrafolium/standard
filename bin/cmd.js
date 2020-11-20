2a7ae8619b8a2de68832b64143354f45f127f78f
Inspecode <inspecode-bot@rocro.com>
2020-11-20 08:50:57 +0000

    Fix PHP_CodeSniffer errors

--- bin/cmd.js
+++ bin/cmd.js
@@ -5,9 +5,10 @@ var major = parseInt(match[1], 10)
 var minor = parseInt(match[2], 10)
 
 if (major >= 9 || (major === 8 && minor >= 10)) {
-  require('standard-engine').cli(require('../options'))
+    require('standard-engine').cli(require('../options'))
 }
 else {
-  console.error(
-      'standard: Node 8.10.0 or greater is required. `standard` did not run.')
+    console.error(
+        'standard: Node 8.10.0 or greater is required. `standard` did not run.'
+    )
 }
