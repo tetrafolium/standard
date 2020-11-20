e472dc45a71300ac51ba44f6848916b2fe110944
Inspecode <inspecode-bot@rocro.com>
2020-11-20 09:40:03 +0000

    Fix ClangFormat errors

--- options.js
+++ options.js
@@ -3,13 +3,11 @@ const path = require('path')
 const pkg = require('./package.json')
 
 module.exports = {
-  bugs: pkg.bugs.url,
-  cmd: 'standard',
+  bugs : pkg.bugs.url,
+  cmd : 'standard',
   eslint,
-  eslintConfig: {
-    configFile: path.join(__dirname, 'eslintrc.json')
-  },
-  homepage: pkg.homepage,
-  tagline: 'Use JavaScript Standard Style',
-  version: pkg.version
+  eslintConfig : {configFile : path.join(__dirname, 'eslintrc.json')},
+  homepage : pkg.homepage,
+  tagline : 'Use JavaScript Standard Style',
+  version : pkg.version
 }
--- test/cmd.js
+++ test/cmd.js
@@ -7,10 +7,8 @@ const CMD_PATH = path.join(__dirname, '..', 'bin', 'cmd.js')
 test('command line usage: --help', t => {
   t.plan(1)
 
-  const child = crossSpawn(CMD_PATH, ['--help'])
-  child.on('error', err => {
-    t.fail(err)
-  })
+const child = crossSpawn(CMD_PATH, [ '--help' ])
+child.on('error', err => {t.fail(err)})
   child.on('close', code => {
     t.equal(code, 0, 'zero exit code')
   })
